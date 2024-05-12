7d0bed2bf05026d4914258e4253906b19628475d
omer110100 <omer110100@gmail.com>
2024-05-12 19:03:23 +0300

    test

--- hr4you.c
+++ hr4you.c
@@ -8,27 +8,34 @@
 //bla2 
 //asd
 bool InputFileIsOpend = false, OutputFileIsOpend = false;
-void ReadFromFile(FILE **FileInput, int FileLocasionInargv, char *argv[], char *Buffer);
-void ExitFile(FILE *FileInput, FILE *FileOtput);
-void MyOpenOutputFile(char *FileName, char Mode, FILE **FileOtput);
+void ReadFromFile(FILE **FileInput,  FILE **FileOutput, int FileLocasionInargv, char *argv[], char *Buffer);
+void ExitFile(FILE *FileInput, FILE *FileOutput);
+void MyOpenOutputFile(char *FileName, char Mode, FILE **FileOutput);
 void MyOpenInputFile(char *FileName, char Mode, FILE **FileInput);
-int ValidInputFile(int argc, char *argv[], FILE **FileInput, FILE **FileOtput);
-void DetectCommands(char *Buffer);
+int ValidInputFile(int argc, char *argv[], FILE **FileInput, FILE **FileOutput);
+void DetectCommands(char *Buffer,  FILE **FileOutput);
+// void AddWorker(char *Buffer);
+// void AddShift(char *Buffer);
+//void ReportShifts(char *Buffer,  FILE **FileOutput);
+void ReportShiftDetails(char *Buffer,  FILE **FileOutput);
+// void ReportWorkers(char *Buffer,  FILE **FileOutput);
+// void RemoveWorker(char *Buffer);
 
 typedef struct Worker
 {
         char name[50];
         int id;
         float hourly_wage;
-        char role[50];
+        worker_role role;
         int number_of_shifts; // 1 <= num <= 7
-        Shift shifts[7];
+        shift_type shift_type[7];
 } Worker;
 
-typedef struct Shift
-{
-        shift_type shift; 
-} Shift;
+// typedef struct Shift
+// {
+//         shift_type shift_type; 
+//         shift_day shift_day; 
+// } Shift;
 
 Worker workers[MAX_WORKERS];
 
@@ -36,14 +43,14 @@ Worker workers[MAX_WORKERS];
 int main(int argc, char *argv[])
 {
         FILE *FileInput = stdin;
-        FILE *FileOtput = stdout;
+        FILE *FileOutput = stdout;
         int FileLocasionInargv = 0;
         char Buffer[MAX_LEN];
-        FileLocasionInargv = ValidInputFile(argc, argv, &FileInput, &FileOtput);
-        ExitFile(FileInput, FileOtput);
-        ReadFromFile(&FileInput, FileLocasionInargv, argv, Buffer);
+        FileLocasionInargv = ValidInputFile(argc, argv, &FileInput, &FileOutput);
+        ExitFile(FileInput, FileOutput);
+        ReadFromFile(&FileInput, &FileOutput, FileLocasionInargv, argv, Buffer);
 }
-int ValidInputFile(int argc, char *argv[], FILE **FileInput, FILE **FileOtput)
+int ValidInputFile(int argc, char *argv[], FILE **FileInput, FILE **FileOutput)
 {
         int FileLocasionInargv = -1;
         for (int i = 1; i < argc; i++)
@@ -64,7 +71,7 @@ int ValidInputFile(int argc, char *argv[], FILE **FileInput, FILE **FileOtput)
                 {
                         if (argv[i + 1] != NULL && strcmp(argv[i + 1], "-i") != 0)
                         {
-                                MyOpenOutputFile(argv[i + 1], 'r', FileOtput);
+                                MyOpenOutputFile(argv[i + 1], 'r', FileOutput);
                         }
                         else
                                 prog2_report_error_message(INVALID_ARGUMENTS);
@@ -86,20 +93,20 @@ void MyOpenInputFile(char *FileName, char Mode, FILE **FileInput)
                 InputFileIsOpend = true;
         }
 }
-void MyOpenOutputFile(char *FileName, char Mode, FILE **FileOtput)
+void MyOpenOutputFile(char *FileName, char Mode, FILE **FileOutput)
 {
-        *FileOtput = fopen(FileName, &Mode);
-        if (*FileOtput == NULL)
+        *FileOutput = fopen(FileName, &Mode);
+        if (*FileOutput == NULL)
         {
                 prog2_report_error_message(CANNOT_OPEN_FILE);
         }
         else
         {
                 OutputFileIsOpend = true;
-                // printf("file Is open : FileOtput\n");
+                // printf("file Is open : FileOutput\n");
         }
 }
-void ExitFile(FILE *FileInput, FILE *FileOtput)
+void ExitFile(FILE *FileInput, FILE *FileOutput)
 {
         if (FileInput == stdin)
         {
@@ -115,13 +122,13 @@ void ExitFile(FILE *FileInput, FILE *FileOtput)
                 }
                 if (OutputFileIsOpend == true)
                 {
-                        fclose(FileOtput);
-                        // printf("file Close :FileOtput\n");
+                        fclose(FileOutput);
+                        // printf("file Close :FileOutput\n");
                 }
         }
 }
 
-void ReadFromFile(FILE **FileInput, int FileLocasionInargv, char *argv[], char *Buffer)
+void ReadFromFile(FILE **FileInput, FILE **FileOutput, int FileLocasionInargv, char *argv[], char *Buffer)
 {
         MyOpenInputFile(argv[FileLocasionInargv], 'r', FileInput);
         while (fgets(Buffer, MAX_LEN, *FileInput) != NULL)
@@ -129,7 +136,7 @@ void ReadFromFile(FILE **FileInput, int FileLocasionInargv, char *argv[], char *
                 // Buffer represents a single row
                 if (Buffer[0] != '\n' && Buffer[0] != '#')
                 {
-                        DetectCommands(Buffer);
+                        DetectCommands(Buffer, FileOutput);
                 }
                 else
                 {
@@ -139,7 +146,7 @@ void ReadFromFile(FILE **FileInput, int FileLocasionInargv, char *argv[], char *
         }
 }
 
-void DetectCommands(char *Buffer)
+void DetectCommands(char *Buffer, FILE **FileOutput)
 {
         char *word = strtok(Buffer, " \n\r");
 
@@ -151,11 +158,11 @@ void DetectCommands(char *Buffer)
                         word = strtok(NULL, " ");
                         if (strcmp(word, "Worker") == 0)
                         {
-                                addWorker(Buffer);
+                                //AddWorker(Buffer);
                         }
                         else if (strcmp(word, "Shift") == 0)
                         {
-                                addShift(Buffer);
+                                //AddShift(Buffer);
                         }
                 }
                 else if (strcmp(word, "Report") == 0)
@@ -164,23 +171,97 @@ void DetectCommands(char *Buffer)
                         word = strtok(NULL, " ");
                         if (strcmp(word, "Shift") == 0)
                         {
-                                reportShiftDetails(Buffer);
+                                ReportShiftDetails(Buffer, FileOutput);
                         }
                         else if (strcmp(word, "Workers") == 0)
                         {
-                                reportWorkers(Buffer);
+                                //ReportWorkers(Buffer);
                         }
                         else if (strcmp(word, "Shifts") == 0)
                         {
-                                reportShifts(Buffer);
+                                //ReportShifts(Buffer);
                         }
                 }
                 else if (strcmp(word, "Remove") == 0)
                 {
                         // renive command
-                        removeWorker(Buffer);
+                        //RemoveWorker(Buffer);
                 }
 
                 word = strtok(NULL, " ");
         }
 }
+void ReportShiftDetails(char *Buffer, FILE **FileOutput)
+{
+        char *word = strtok(Buffer, " \n\r");
+        shift_type type;
+        
+        while (word != NULL)
+        {
+                
+                if (strcmp(word, "Details") == 0)
+                {
+                        
+                        int passValid = 1;
+
+                        word = strtok(NULL, " ");
+                        int day = atoi(word);
+                        if( day < 1 || day > 7 )
+                        {
+                                prog2_report_error_message(INVALID_SHIFT_DAY);
+                                passValid = 0;
+                        }
+                        else{
+                                shift_day day = day;
+                        }
+
+                        word = strtok(NULL, " ");
+
+                        if (strcmp(word, "Morning") == 0)
+                        {
+                                type = MORNING;
+                        }
+                        else if (strcmp(word, "Afternoon") == 0)
+                        {
+                                type = AFTERNOON;
+                        }
+                        else if (strcmp(word, "Evening") == 0)
+                        {
+                                type = EVENING;
+                        }
+                        else if (strcmp(word, "Night") == 0)
+                        {
+                                type = NIGHT;
+                        }
+                        else{
+                                prog2_report_error_message(INVALID_SHIFT_TYPE);
+                                passValid = 0;
+                        }
+
+                        if(passValid)
+                        {
+                                int num_of_workers = 0;
+                                int total_payment = 0;
+                                
+                                for (size_t i = 0; i < MAX_WORKERS; i++)
+                                {
+                                        if(workers[i].id){
+                                                //עובד רלוונטי לבדיקה
+                                                if(workers[i].shift_type[day] == type)
+                                                {
+                                                        num_of_workers++;
+                                                        total_payment += workers[i].hourly_wage;
+                                                }    
+                                        }
+                                }
+                                
+                                // void prog2_report_shift_details(FILE *out, shift_day day, shift_type type, int num_of_workers, float total_payment);
+                                prog2_report_shift_details(*FileOutput, day, type, num_of_workers, total_payment);
+                        }
+
+
+                }
+                word = strtok(NULL, " ");
+        }
+}
+
